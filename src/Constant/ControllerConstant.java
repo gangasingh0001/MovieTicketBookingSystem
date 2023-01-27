14ff9cf0e97f6a4fd960373200c0eab0dd680903
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-26 20:25:42 -0500

    Added method names as constants in new file named ControllerConstant

---
+++ src/Constant/ControllerConstant.java
@@ -0,0 +1,10 @@
+package Constant;
+
+public class ControllerConstant {
+    public static final String addMovieSlots = "addMovieSlots";
+    public static final String removeMovieSlots = "removeMovieSlots";
+    public static final String listMovieShowsAvailability = "listMovieShowsAvailability";
+    public static final String bookMovieTickets = "bookMovieTickets";
+    public static final String getBookingSchedule = "getBookingSchedule";
+    public static final String cancelMovieTickets = "cancelMovieTickets";
+}
