6c1b9a273da731722e2f464320630a06022c7027
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-26 19:26:11 -0500

    Added server details in constants

--- src/Constant/ServerConstant.java
+++ src/Constant/ServerConstant.java
@@ -1,4 +1,13 @@
 package Constant;
 
 public class ServerConstant {
+    public static final int SERVER_ATWATER_PORT = 2501;
+    public static final int SERVER_VERDUN_PORT = 2502;
+    public static final int SERVER_OUTREMONT_PORT = 2503;
+    public static final String SERVER_ATWATER_PREFIX = "ATWA";
+    public static final String SERVER_VERDUN_PREFIX = "VER";
+    public static final String SERVER_OUTREMONT_PREFIX = "OUT";
+    public static final String SERVER_ATWATER = "ATWATER";
+    public static final String SERVER_VERDUN = "VERDUN";
+    public static final String SERVER_OUTREMONT = "OUTREMONT";
 }
