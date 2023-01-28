c27223eae3ed3628ca15be41f3404970adc213d7
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-28 01:24:05 -0500

    Fixed override method exception

--- src/Controller/MovieTicketController.java
+++ src/Controller/MovieTicketController.java
@@ -10,34 +10,27 @@ public class MovieTicketController extends UnicastRemoteObject implements MovieT
         super();
     }
 
-    @Override
-    public String addMovieSlots(String movieId, String movieName, Integer bookingCapacity) throws RemoteException {
-
+    public String addMovieSlots(String movieId, String movieName, int bookingCapacity) throws RemoteException {
         return null;
     }
 
-    @Override
     public String removeMovieSlots(String movieId, String movieName) throws RemoteException {
         return null;
     }
 
-    @Override
     public String listMovieShowsAvailability(String movieName) throws RemoteException {
         return null;
     }
 
-    @Override
-    public String bookMovieTickets(String customerID, String movieId, String movieName, String numberOfTickets) throws RemoteException {
+    public String bookMovieTickets(String customerID, String movieId, String movieName, int numberOfTickets) throws RemoteException {
         return null;
     }
 
-    @Override
     public String getBookingSchedule(String customerID) throws RemoteException {
         return null;
     }
 
-    @Override
-    public String cancelMovieTickets(String customerID, String movieID, String movieName, Integer numberOfTickets) throws RemoteException {
+    public String cancelMovieTickets(String customerID, String movieID, String movieName, int numberOfTickets) throws RemoteException {
         return null;
     }
 }
