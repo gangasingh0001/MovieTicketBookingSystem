97d741b2c47d7b2de0a184c66b27eca01d73575b
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-28 01:30:15 -0500

    Rebind Registry

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -3,12 +3,12 @@ package Server;
 import Constant.ControllerConstant;
 import Constant.ServerConstant;
 import Controller.MovieTicketController;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -22,25 +22,27 @@ public class Server extends Thread{
     private static int serverPort;
 
     public Server (String serverID) throws Exception{
+        System.out.println("Server ID " + serverID);
         this.serverID = serverID;
     }
 
-    public static void main(String args[]) {
-        getServerInfo();
-    }
+    public static void main(String args[]) {}
 
-    private void runServer() throws RemoteException, AlreadyBoundException {
+    public void runServer() throws RemoteException {
         movieTicketController = new MovieTicketController();
         startRegistry();
         startThread();
     }
 
-    private static void startRegistry() throws RemoteException {
+    private void startRegistry() {
         try {
-            Registry registry = LocateRegistry.getRegistry(serverPort);
-            if(registry.list().length==0)  LocateRegistry.createRegistry(serverPort);
-        } catch (RemoteException ex) {
-            // TODO: Write Exception handling for startRegistry method.
+            System.out.println("Getting Server Port " + serverPort);
+            Registry registry = LocateRegistry.createRegistry(serverPort);
+            registry.rebind(ControllerConstant.MovieTicketController,movieTicketController);
+        } catch (AccessException e) {
+            throw new RuntimeException(e);
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -52,7 +54,7 @@ public class Server extends Thread{
         thread.start();
     }
 
-    private static void getServerInfo() {
+    public void getServerInfo() {
         switch(serverID) {
             case ServerConstant.SERVER_ATWATER_PREFIX:
                 serverName = ServerConstant.SERVER_ATWATER;
@@ -78,13 +80,17 @@ public class Server extends Thread{
             socket = new DatagramSocket(serverPort);
             byte[] buffer = new byte[1000];
             while (true) {
+                System.out.println("Request");
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 socket.receive(request);
                 String requestParams = new String(request.getData(), 0,
                         request.getLength());
                 String[] requestParamsArray = requestParams.split(";");
-                String invokedMethod = requestParamsArray[0];
-                response = methodInvocation(invokedMethod,response);
+//                String customerID = requestParamsArray[1];
+//                String movieType = requestParamsArray[2];
+//                String movieID = requestParamsArray[3];
+//                String invokedMethod = requestParamsArray[0];
+//                response = methodInvocation(invokedMethod,response);
                 byte[] sendData = response.getBytes();
                 DatagramPacket reply = new DatagramPacket(sendData, response.length(), request.getAddress(),
                         request.getPort());
@@ -100,16 +106,16 @@ public class Server extends Thread{
         }
     }
 
-    private String methodInvocation(@NotNull String invokedMethod, String response) {
-        if (invokedMethod.equalsIgnoreCase(ControllerConstant.removeMovieSlots)) {
-            response = movieTicketController.removeMovieSlots();
-        } else if (invokedMethod.equalsIgnoreCase(ControllerConstant.bookMovieTickets)) {
-            response = movieTicketController.bookMovieTickets();
-        } else if (invokedMethod.equalsIgnoreCase(ControllerConstant.cancelMovieTickets)) {
-            response = movieTicketController.cancelMovieTickets();
-        } else if (invokedMethod.equalsIgnoreCase(ControllerConstant.addMovieSlots)) {
-            response = movieTicketController.addMovieSlots();
-        }
-        return response;
-    }
+//    private String methodInvocation(@NotNull String invokedMethod, String response) {
+//        if (invokedMethod.equalsIgnoreCase(ControllerConstant.removeMovieSlots)) {
+//            response = movieTicketController.removeMovieSlots();
+//        } else if (invokedMethod.equalsIgnoreCase(ControllerConstant.bookMovieTickets)) {
+//            response = movieTicketController.bookMovieTickets();
+//        } else if (invokedMethod.equalsIgnoreCase(ControllerConstant.cancelMovieTickets)) {
+//            response = movieTicketController.cancelMovieTickets();
+//        } else if (invokedMethod.equalsIgnoreCase(ControllerConstant.addMovieSlots)) {
+//            response = movieTicketController.addMovieSlots();
+//        }
+//        return response;
+//    }
 }
