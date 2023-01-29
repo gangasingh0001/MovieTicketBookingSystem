ba1c846693aec0c2e70e124d01f5ecb950b5d352
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-29 14:36:23 -0500

    rename controller constant to service constant

--- src/Constant/ServiceConstant.java
+++ src/Constant/ServiceConstant.java
@@ -1,6 +1,8 @@
 package Constant;
 
-public class ControllerConstant {
+import java.rmi.RemoteException;
+
+public class ServiceConstant {
     /**
      * Class name constant
      */
@@ -11,4 +13,8 @@ public class ControllerConstant {
     public static final String bookMovieTickets = "bookMovieTickets";
     public static final String getBookingSchedule = "getBookingSchedule";
     public static final String cancelMovieTickets = "cancelMovieTickets";
+    public static final String getMoviesListInTheatre = "getMoviesListInTheatre";
+    public static final String bookTicket = "bookTicket";
+    public static final String getCustomerBookingList = "getCustomerBookingList";
+    public static final String cancelTicket = "cancelTicket";
 }
