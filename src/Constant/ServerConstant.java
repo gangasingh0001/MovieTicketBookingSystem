dd46a3c288d505ad46e93b28466ba8df8766130d
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-29 14:35:38 -0500

    Added some server constants

--- src/Constant/ServerConstant.java
+++ src/Constant/ServerConstant.java
@@ -10,4 +10,8 @@ public class ServerConstant {
     public static final String SERVER_ATWATER = "ATWATER";
     public static final String SERVER_VERDUN = "VERDUN";
     public static final String SERVER_OUTREMONT = "OUTREMONT";
+    public static final String ADD_MOVIE_SLOTS = "1.Add Movie Slots";
+    public static final String REMOVE_MOVIE_SLOTS = "2.Remove Movie Slots";
+    public static final String LIST_MOVIE_SHOWS_AVAILABILITY = "3.List Movie Shows Availability";
+    public static final String LOGOUT = "4. LOGOUT";
 }
