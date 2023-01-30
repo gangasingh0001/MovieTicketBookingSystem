ee4baf8bc9a0687fa36e979fca20a2b282b9ea0d
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-01-30 02:31:56 -0500

    Added test data

--- src/Server/ServerInstance.java
+++ src/Server/ServerInstance.java
@@ -1,6 +1,10 @@
 package Server;
 
 import Constant.ServerConstant;
+import Shared.Database.CustomerBooking;
+import Shared.Database.ICustomerBooking;
+import Shared.Database.IMovies;
+import Shared.Database.Movies;
 import Shared.data.*;
 
 public class ServerInstance {
@@ -9,12 +13,18 @@ public class ServerInstance {
         IServerInfo serverInfo;
         IUdp udpService;
         IMovie movieService;
+        ICustomerBooking customerBookingDb;
+        IMovies moviesDb;
         try {
             serverInfo = new ServerInfo();
             udpService = new Udp();
             movieService = new Movie();
+            customerBookingDb = new CustomerBooking();
+            moviesDb = new Movies();
+            moviesDb.addMovie("AVATAR","ATWE190822",50);
+            customerBookingDb.addMovieByCustomerID("ATWA1212","ATWE190822","AVATAR",0);
             serverInfo.setServerName(ServerConstant.SERVER_ATWATER_PREFIX);
-            Server Atwater = new Server(ServerConstant.SERVER_ATWATER_PREFIX,serverInfo,udpService,movieService);
+            Server Atwater = new Server(ServerConstant.SERVER_ATWATER_PREFIX,serverInfo,udpService,movieService,customerBookingDb,moviesDb);
             Atwater.getServerInfo();
             Atwater.runServer();
         } catch (Exception e) {
@@ -25,8 +35,12 @@ public class ServerInstance {
             serverInfo = new ServerInfo();
             udpService = new Udp();
             movieService = new Movie();
+            customerBookingDb = new CustomerBooking();
+            moviesDb = new Movies();
+            moviesDb.addMovie("AVATAR","VERM190822",30);
+            moviesDb.addMovie("AVENGERS","VERM190822",40);
             serverInfo.setServerName(ServerConstant.SERVER_VERDUN_PREFIX);
-            Server Verdun = new Server(ServerConstant.SERVER_VERDUN_PREFIX,serverInfo,udpService,movieService);
+            Server Verdun = new Server(ServerConstant.SERVER_VERDUN_PREFIX,serverInfo,udpService,movieService,customerBookingDb,moviesDb);
             Verdun.getServerInfo();
             Verdun.runServer();
         } catch (Exception e) {
@@ -37,8 +51,11 @@ public class ServerInstance {
             serverInfo = new ServerInfo();
             udpService = new Udp();
             movieService = new Movie();
+            customerBookingDb = new CustomerBooking();
+            moviesDb = new Movies();
+            moviesDb.addMovie("TITANIC","OUTM190822",30);
             serverInfo.setServerName(ServerConstant.SERVER_OUTREMONT_PREFIX);
-            Server Outremont = new Server(ServerConstant.SERVER_OUTREMONT_PREFIX,serverInfo,udpService,movieService);
+            Server Outremont = new Server(ServerConstant.SERVER_OUTREMONT_PREFIX,serverInfo,udpService,movieService,customerBookingDb,moviesDb);
             Outremont.getServerInfo();
             Outremont.runServer();
         } catch (Exception e) {
